5f4550bd50b249384db3a6720be5e4472f06e165
Aleksandr Viznyi <senak98@yandex.ru>
2024-06-20 19:47:38 +0300

    add styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
-import './App.style.scss'
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-
+import "./App.style.scss";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import Header from "./Header/Header";
 
 export function App() {
-    const params = Object.entries({
-        cc_load_policy: 0,
-        controls: 2,
-        fs: 0,
-        rel: 0,
-        showinfo: 0
-    }).map(([key, value]) => `${key}=${value}`).join('&')
+  const params = Object.entries({
+    cc_load_policy: 0,
+    controls: 2,
+    fs: 0,
+    rel: 0,
+    showinfo: 0,
+  })
+    .map(([key, value]) => `${key}=${value}`)
+    .join("&");
 
-    return (<div className='App'>
-
-    </div>)
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,9 +1,15 @@
-import AppsIcon from '@mui/icons-material/Apps';
+import AppsIcon from "@mui/icons-material/Apps";
+import ReplyIcon from "@mui/icons-material/Reply";
+import "./Header.style.scss";
+
 
 export default function Header() {
-    return (
-        <header className="header">
-            <AppsIcon />
-        </header>
-    )
+  return (
+    <header className="header">
+      <AppsIcon className="header__icon" sx={{ color: "#D3D3D3" }} />
+      <ReplyIcon className="header__icon" sx={{ color: "#D3D3D3" }} />
+      <p className="header__text">Просмотр</p>
+      <p className="header__text">Управление</p>
+    </header>
+  );
 }
